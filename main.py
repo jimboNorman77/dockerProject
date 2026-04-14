4b593a6885843391fd322a79d79f4f2b4c4a00e3
jimboNorman77 <134060369+jimboNorman77@users.noreply.github.com>
2026-04-14 12:15:09 +0100

    Started Working with static analysis of containers
    Just for safe keeping

---
+++ main.py
@@ -0,0 +1,123 @@
+import sys
+import os
+import subprocess
+import json
+
+
+flags = ""
+modes = ["static", "hybrid", "active"]
+
+def verboseFlag(verbose):
+	if verbose:
+		print("Verbose Mode Engaged")
+
+def activeFlag(active):
+	if active:
+		print("Active Mode Engaged")
+
+def passiveFlag(passive):
+	if passive:
+		print("Passive Mode Engaged")
+
+def createSBOM(container):
+	command = "docker run -v ./output:/output aquasec/trivy:canary image -q --scanners vuln --format cyclonedx --output /output/result.json " + container
+	check = subprocess.run(command, shell=True) 
+
+def readJSON():
+	with open("./output/result.json") as jsonfile:
+		d = json.load(jsonfile)
+		print(d)
+
+def static(container):
+	createSBOM(container)
+	readJSON()
+
+
+def main(mode, container):
+	##print(active)
+	##print(passive)
+	##print(verbose)
+	print(mode)
+	print(container)
+	##Runs command to check for container among running containers
+	output = subprocess.check_output("sudo docker ps", shell=True)
+	output = output.decode("UTF-8")
+	if container in output:
+		print("This container exists")
+	else:
+		print("This container does not exist")
+	print(output[2])
+
+##Detects if the script is being run or the user wants to access the help manual.
+if sys.argv[1] in ["help", "-h"]:
+	##Prints the help manual with all modes and flags which can be used.
+	print('''Input should be structured python3 [mode] [container] [flags]
+	         Where \n
+		 [mode] == What mode you want to run the program in \n
+		 [container] == The image name of the container \n
+	         [flags] == The flags you want to run the program with \n
+		 Modes \n
+ 		 \t Scan \n
+		 \t DockerFile \n
+		 Flags \n
+		 \t -a Active \n
+		 \t -p Passive \n
+	         \t -v Verbose''')
+	##Quits the program after printing the help manual
+	os._exit(1)
+else:
+	##Checks to see if the mode entered is a valid mode.
+	try:
+		for m in modes:
+			if sys.argv[1] == m:
+				mode = sys.argv[1]
+		if mode != sys.argv[1]:
+			print("Invalid Mode Selected")
+			os._exit(1)
+	##Stores the mode into the variable mode
+	except:
+		print("Invalid Mode Selected")
+		os._exit(1)
+	##Checks to see if a valid running container has been selected
+	try:
+		containerInfo = subprocess.check_output("docker ps", shell=True)
+		containerInfo = containerInfo.decode()
+		containerInfo = containerInfo.split("   ")
+		containerInfo = containerInfo[17:]
+		if sys.argv[2] == containerInfo[1]:
+			container = containerInfo[1]
+		else:
+			print("Container Either Invalid or Not Running")
+			os._exit(1)
+	except:
+		print("Container Either Invalid or Not Running")
+		os._exit(1)
+	##Tries to store the flags if any are present
+	try:
+		flags = sys.argv[3]
+	except:
+		print("No Flags Selected, Script Will Run in Default Mode")
+
+
+##for x in flags:
+##	print(x)
+
+##Checks to see if the user has submitted any flags
+if flags != " ":
+	flags = flags[1:]
+	if "a" in flags:
+		active = True
+	if "p" in flags:
+		passive = True
+	if "v" in flags:
+		verbose = True
+
+print(container)
+print(mode)
+
+##activeFlag(active)
+##passiveFlag(passive)
+##verboseFlag(verbose)
+##if mode == "static":
+##	static(container)
+
