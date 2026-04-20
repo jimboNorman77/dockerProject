fc3a98db2c194bb2ec56cae63bd815f506de92c8
jimboNorman77 <134060369+jimboNorman77@users.noreply.github.com>
2026-04-20 11:24:53 +0100

    Cleaned up code so it actually works

--- testcode.py
+++ testcode.py
@@ -1,9 +1,11 @@
 file = "/etc/passwd"
 ##for file in files_to_check:
-command = "FILE=" + file
+command = "FILE=" + file + "\n"
 with open("script.sh", "w") as f:
-     f.write("#!/bin/bash")
+     f.write("#!/bin/bash\n")
      f.write(command)
-     f.write("if -w $FILE; then")
-     f.write("\techo '$FILE is writeable'")
+     f.write("if [ -w $FILE ]; then\n")
+     f.write('\techo "$FILE is writeable"\n')
+     f.write('else\n')
+     f.write("\techo "$FILE is not writeable"\n')
      f.write("fi")
